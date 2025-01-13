7f1250b062f5fc35d196810d6dd0ad18bf96a80f
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2025-01-13 18:31:14 +0400

    Adding Google authentication

--- BookNest/App/AppDelegate.swift
+++ BookNest/App/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import GoogleSignIn
 import FirebaseCore
 
 @main
@@ -9,6 +10,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
