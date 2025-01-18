5cfb523e2425a5f730cf773bcc8ab18a03a48d04
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2025-01-18 14:24:04 +0400

    Adding TabBar

---
+++ BookNest/Screens/BooksPage/BooksView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct BooksView: View {
+    var body: some View {
+        NavigationView {
+            Text("This is the Books View")
+                .font(.title)
+                .navigationTitle("Books")
+        }
+    }
+}
---
+++ BookNest/Screens/ProfilePage/ProfileView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        NavigationView {
+            Text("This is the Profile View")
+                .font(.title)
+                .navigationTitle("Profile")
+        }
+    }
+}
